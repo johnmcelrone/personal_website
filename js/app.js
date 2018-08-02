df42693cbf32397d34145bb422cab24a10785854
John McElrone <johnmcelrone@Johns-MBP.fios-router.home>
2018-08-02 11:41:21 -0400

    project section draft

--- js/app.js
+++ js/app.js
@@ -8,21 +8,39 @@ const sectionMenu = $('.section-menu');
 const sectionMenuShow = $('.section-menu-show');
 const sectionIntro = $('.section-intro');
 const navMenu = $('.nav-menu');
+const sectionProj = $('.section-proj');
+
+const divIntro = $('.intro-parent');
+const rowOne = $('.intro-row-1');
+const rowTwo = $('.intro-row-2');
+const rowThree = $('.intro-row-3');
+const rowFour = $('.intro-row-4');
+const rowFive = $('.intro-row-5');
+
+const headerJohn = $('.h1-john');
+const headerDes = $('.h1-des');
+const headerDev = $('.h1-dev');
+const headerLight = $('.h1-pad');
+
+window.onload = function () {
+    divIntro.hide().delay(1000).fadeIn(1550);
+    headerLight.delay(2600).animate({width: '97%'}, 673);
+}
 
 $(document).ready(function(){
     $("li.jcon-menuopen").click(function(){
         $("section.section-menu").toggleClass("section-menu-show");
         
         if($('.section-menu-show').css('display') == 'flex') {
-            navJcons.css('color', 'var(--light)');
-            domHTML.css('background', 'var(--dark)');
             domBody.css('overflow', 'hidden');
-            navMenu.css('display', 'flex');
+            navMenu.fadeIn(2000).css('display', 'block');
+            navJcons.css('color', 'var(--light)');
+            sectionProj.css('display', 'none');
         } else {
-            navJcons.css('color', 'var(--dark)');
-            domHTML.css('background', 'var(--light)');
             domBody.css('overflow-y', 'scroll');
             navMenu.css('display', 'none');
+            navJcons.css('color', 'var(--dark)');
+            sectionProj.css('display', 'flex');
         }
     });
 });
